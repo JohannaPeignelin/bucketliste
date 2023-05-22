74330bda0e537b2c3192c30390e293a86b782bbd
Johanna Peignelin <johanna.peignelin2023@campus-eni.fr>
2023-05-22 14:47:21 +0200

    Installation du projet

--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -3,9 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Wish;
+use App\Form\WishType;
 use App\Repository\WishRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 #[Route('/wish', name: 'wish_')]
 class WishController extends AbstractController
@@ -34,6 +36,35 @@ class WishController extends AbstractController
         //TODO renvoyer le détail d'un item de la Bucket Liste
         return $this->render('wish/show.html.twig',[
             'wish' => $wish
+        ]);
+    }
+
+    #[Route('/add', name: 'add')]
+    public function add(Request $request, WishRepository $wishRepository): Response
+    {
+
+        $wish = new Wish();
+
+        //instanciation du formulaire en lui passant l'interface de Wish
+        $wishForm = $this->createForm(WishType::class,$wish);
+
+        //permet d'extraire les données de la requête
+        $wishForm->handleRequest($request);
+
+        //on rentre dans le if si appui sur submit et TODO  si les asserts sont valides
+        if ($wishForm->isSubmitted() && $wishForm->isValid()){
+            //traitement de la donnée
+            //enregistre le wish dans la BDD
+
+            $wishRepository->save($wish, true);
+
+            //redirige vers la page de détails avec message de confirmation
+            $this->addFlash('success', 'Idea successfully added ! ');
+            return $this->redirectToRoute('wish_show', ['id' => $wish->getId()]);
+        }
+
+        return $this->render('wish/add.html.twig', [
+            'wishForm' => $wishForm->createView()
         ]);
     }
 
--- src/Entity/Wish.php
+++ src/Entity/Wish.php
@@ -6,8 +6,10 @@ use App\Repository\WishRepository;
 use DateTimeInterface;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: WishRepository::class)]
+#[ORM\HasLifecycleCallbacks]
 class Wish
 {
     #[ORM\Id]
@@ -15,12 +17,28 @@ class Wish
     #[ORM\Column]
     private ?int $id = null;
 
+
+    #[Assert\NotBlank(message: "A title is mandatory")]
+    #[Assert\Length(
+        min: 2,
+        max: 250,
+        minMessage: "Minimum {{ limit }} character !",
+        maxMessage: "Maximum {{ limit }} character ! "
+
+    )]
     #[ORM\Column(length: 250)]
     private ?string $title = null;
 
+    #[Assert\Length(
+        min: 10,
+        max: 4000,
+        minMessage: "Minimum {{ limit }} character please!",
+        maxMessage: "Maximum {{ limit }} characters please !"
+    )]
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $description = null;
 
+    #[Assert\NotBlank(message: "An author is mandatory")]
     #[ORM\Column(length: 50)]
     private ?string $author = null;
 
@@ -93,5 +111,12 @@ class Wish
         $this->dateCreated = $dateCreated;
 
         return $this;
+    }
+
+    #[ORM\PrePersist]
+    public function setNewWish(){
+        $this->setDateCreated(new \DateTime());
+        $this->setIsPublished(true);
+
     }
 }
